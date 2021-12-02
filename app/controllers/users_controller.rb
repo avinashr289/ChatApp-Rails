62ef8f17e4a8d76b7d2b9a69c564c9c7d2059409
avinash <avinash@magiccrate.in>
2021-12-02 12:40:43 +0530

    Sign up page and vaidations

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+
+    def new
+        @user=User.new
+    end
+
+    def create
+        @user=User.new(user_params)
+        
+        p_mismatch=false
+        if(params[:session][:password]!=params[:session][:cpassword])
+            p_mismatch=true 
+        end
+
+        
+        if(!p_mismatch && @user.save)
+            
+            flash[:success]="User has been created"
+            session[:user_id]=@user.id
+            redirect_to root_path
+        else
+        
+            if(p_mismatch)
+                @user.errors.add(:base,"Password did not match..Try again!")
+            end
+
+            render 'new'
+        end
+    end
+
+    private 
+
+    def user_params
+        params.require(:session).permit(:username,:password)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   post 'login',:to => "sessions#create"
   delete 'logout',:to => "sessions#destroy"
 
+  get 'signup',:to => "users#new"
+  post 'signup',:to => "users#create"
+
+
   post 'message',:to => "messages#create"
 
   mount ActionCable.server ,at: '/cable'
